e444c4a1effee17827f68c6582603b81da3b6d94
kashishverma-droid <kashish.verma@3pillarglobal.com>
2025-10-26 18:43:48 +0530

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.awt.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main{
+    public static void main(String[] args) {
+
+        WebDriver driver= new ChromeDriver();
+    //    driver.navigate("https://www.amazon.in/");//navigate
+        driver.navigate().to("https://www.amazon.in/");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("i phone");
+        driver.findElement(By.id("twotabsearchtextbox")).submit();
+        driver.findElement(By.id("nav-cart"));
+        driver.findElement(By.id("nav-cart")).click();
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.close();
+        driver.quit();
+
+
+    }
+
+}
+
