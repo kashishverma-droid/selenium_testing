7646cb9cce112b97ffbd5b09b59eb035e3235312
kashishverma-droid <kashish.verma@3pillarglobal.com>
2025-10-26 18:50:13 +0530

    Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,14 +7,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.awt.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main{
     public static void main(String[] args) {
 
         WebDriver driver= new ChromeDriver();
-    //    driver.navigate("https://www.amazon.in/");//navigate
-        
+    
+        driver.navigate().to("https://www.amazon.in/");
         driver.manage().window().maximize();
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("i phone");
         driver.findElement(By.id("twotabsearchtextbox")).submit();
